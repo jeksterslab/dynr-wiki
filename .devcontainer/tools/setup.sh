22442d68bd435d869db8ec1ce7e38ab9f883aa31
jeksterslab <learn.jeksterslab@gmail.com>
2023-07-05 17:10:29 +0000

    Automated build.

--- .devcontainer/tools/setup.sh
+++ .devcontainer/tools/setup.sh
@@ -25,7 +25,8 @@ apt_install python3-pip
 pip3 install -U radian
 
 # To add additional R packages,
-# for example, to install tidyverse and dplyr, do the following:
+# for example, to install tidyverse and dplyr
+# do the following:
 install2.r --error --skipinstalled -n -1 \
   tidyverse \
   dplyr
